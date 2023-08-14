2fd42b49533d76bddad7b6607d481465be872dd7
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-08-14 21:14:47 +0300

    Finished the setup for the project

---
+++ GlobeWander/GlobeWander/Data/GlobeWanderDbContext.cs
@@ -0,0 +1,66 @@
+﻿using GlobeWander.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GlobeWander.Data
+{
+    public class GlobeWanderDbContext : DbContext
+    {
+        public GlobeWanderDbContext(DbContextOptions option) : base(option)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<HotelRoom>().HasKey(
+                hotelRooms => new {
+                    hotelRooms.HotelID,
+                    hotelRooms.RoomNumber
+                }
+                );
+
+            modelBuilder.Entity<BookingRoom>()
+                .HasOne(b => b.HotelRooms)  // BookingRoom references HotelRoom
+                .WithOne(h => h.BookingRoom) // HotelRoom references BookingRoom
+                .HasForeignKey<HotelRoom>(h => new { h.HotelID, h.RoomNumber }) // Define the composite foreign key
+                .HasPrincipalKey<BookingRoom>(b => new { b.HotelID, b.RoomNumber });
+
+
+            modelBuilder.Entity<Rate>().HasKey(
+                rate => new
+                {
+                    rate.ID,
+                    rate.TripID
+                });
+            modelBuilder.Entity<BookingTrip>().HasKey(
+                bookingTrip => new
+                {
+                    bookingTrip.ID,
+                    bookingTrip.TripID
+                });
+
+            
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<TourSpot> TourSpots { get; set;}
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+
+        public DbSet<Rate> Rates { get; set; }
+
+        public DbSet<BookingRoom> BookingRooms { get; set; }
+
+        public DbSet<BookingTrip>  bookingTrips { get; set; }
+
+
+
+    }
+}
