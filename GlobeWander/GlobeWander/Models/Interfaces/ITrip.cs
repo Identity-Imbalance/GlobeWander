f406052d6df5fc42725fd42c5e0a6941c9d9a162
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-08-15 13:40:45 +0300

    implmeted  trip + tourspot services and controllers and interface

--- GlobeWander/GlobeWander/Models/Interfaces/ITrip.cs
+++ GlobeWander/GlobeWander/Models/Interfaces/ITrip.cs
@@ -2,5 +2,15 @@
 {
     public interface ITrip
     {
+        public Task<List<Trip>> GetAllTrips();
+
+        public Task<Trip> GetTripByID(int id);
+
+        public Task<Trip> CreateTrip(Trip trip);
+
+        public Task<Trip> UpdateTrip(Trip trip, int id);
+
+        public Task DeleteTrip(int id);
+
     }
 }
