05a9cd0e09e5b70f6f7a5fa4de1b9088fa78d3ee
Bashar Nidal Shukri Shehadeh <1934674@std.hu.edu.jo>
2023-08-15 21:46:03 +0300

    pullDTO

--- GlobeWander/GlobeWander/obj/Debug/net7.0/GlobeWander.AssemblyInfo.cs
+++ GlobeWander/GlobeWander/obj/Debug/net7.0/GlobeWander.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GlobeWander")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d69793c5bfa4ef7a893da93f5ca11c64dd2446")]
 [assembly: System.Reflection.AssemblyProductAttribute("GlobeWander")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GlobeWander")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
