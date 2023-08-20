744ba4d12e3e9d1545124a2b7ab11c5960a5077c
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-08-20 19:12:31 +0300

    booking  trip Done :D

--- GlobeWander/GlobeWander/Migrations/20230820104215_newUpdateOnTripandBookingTrip.Designer.cs
+++ GlobeWander/GlobeWander/Migrations/20230820104215_newUpdateOnTripandBookingTrip.Designer.cs
@@ -4,6 +4,7 @@ using GlobeWander.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GlobeWander.Migrations
 {
     [DbContext(typeof(GlobeWanderDbContext))]
-    partial class GlobeWanderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230820104215_newUpdateOnTripandBookingTrip")]
+    partial class newUpdateOnTripandBookingTrip
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
