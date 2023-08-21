2799340ecec37cec77ea5d4e6d95e058c8992373
Abdallah Alomari <70644811+Abdallahalomarii@users.noreply.github.com>
2023-08-21 13:11:11 +0300

    Update UnitTest1.cs

--- GlobeWander/Test/UnitTest1.cs
+++ GlobeWander/Test/UnitTest1.cs
@@ -632,36 +632,7 @@ public async Task UpdateTourSpot()
             Assert.Null(deletedHotRoom);
 
         }
-        [Fact]
-        public async void enrol_Room_method()
-        {
-            var room = await CreateandSaveRoom();
-
-
-            var service = new RoomService(_db);
-            var actRoom = await service.GetRoomId(room.ID);
-            Assert.NotNull(actRoom);
-            Assert.Equal(room.ID, actRoom.ID);
-            Assert.Equal(room.Name, actRoom.Name);
-            Assert.Equal(room.Layout, actRoom.Layout);
-        }
-       
- 
-        [Fact]
-        public async void enrol_HotelRoom_method()
-        {
-            var hotelRoom = await CreateandSaveHotelRoom();
-            var service = new HotelRoomService(_db);
-            var actHotelRoom = await service.GetHotelRoomId(hotelRoom.HotelID ,hotelRoom.RoomNumber);
-            Assert.NotNull(actHotelRoom);
-            Assert.Equal(hotelRoom.RoomNumber, actHotelRoom.RoomNumber);
-            Assert.Equal(hotelRoom.RoomID, actHotelRoom.RoomID);
-            Assert.Equal(hotelRoom.HotelID , actHotelRoom.HotelID);
-            Assert.Equal(hotelRoom.PricePerDay, actHotelRoom.PricePerDay);
-            
-
-
-        }
+        
 
     }
 }
